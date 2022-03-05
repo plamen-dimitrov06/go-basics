c6186a808be3469b1ff1f887d65107c416167e21
Plamen Dimitrov <plamen.dimitrov06@abv.bg>
2022-03-05 16:53:45 +0200

    complete a skipped task

--- Labs/Loops/evenPowerOfTwo.go
+++ Labs/Loops/evenPowerOfTwo.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
-	// skipped for now
+	var numbersTotal int
+	fmt.Scanln(&numbersTotal)
+
+	fmt.Println(1)
+	for i := 1; i <= numbersTotal; i++ {
+		if i % 2 == 0 {
+			fmt.Println(math.Pow(2, float64(i)))
+		}
+	}
 }
